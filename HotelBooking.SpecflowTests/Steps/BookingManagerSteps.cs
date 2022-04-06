35fd1066a4230593bc3512171a33cdf112332f38
SzymON <42994488+SzymonSoczewka@users.noreply.github.com>
2022-04-06 13:21:19 +0200

    specflow tests

--- HotelBooking.SpecflowTests/Steps/BookingManagerSteps.cs
+++ HotelBooking.SpecflowTests/Steps/BookingManagerSteps.cs
@@ -11,28 +11,28 @@ namespace HotelBooking.SpecflowTests.Steps
     public class BookingManagerSteps
     {
         private IBookingManager bookingManager;
-        private DateTime start;
-        private DateTime end;
-        private static DateTime occupiedStartDate;
-        private static DateTime occupiedEndDate;
-        private static readonly int customerId = 1;
-        private static readonly int roomID = 1;
-        private static readonly int id = 2;
+        private DateTime occupiedDateStart;
+        private DateTime occupiedDateEnd;
+        private static int occupiedNumStart = 10;
+        private static int occupiedNumEnd = 20;
+        private static int customerId = 1;
+        private static int roomID = 1;
+        private static int id = 2;
         private bool result;
 
         private Mock<IRepository<Booking>> mockBookingRepository;
         private Mock<IRepository<Room>> mockRoomRepository;
-        private readonly Mock<IRepository<Customer>> mockCustomerRepository;
-        private readonly Mock<IBookingManager> mockBookingManager;
+        private Mock<IRepository<Customer>> mockCustomerRepository;
+        private Mock<IBookingManager> mockBookingManager;
 
         private IBookingManager fakeBookingManager;
 
         public BookingManagerSteps()
         {
-            occupiedStartDate = DateTime.Today.AddDays(10);
-            occupiedEndDate = DateTime.Today.AddDays(20);
+            occupiedDateStart = DateTime.Today.AddDays(occupiedNumStart);
+            occupiedDateEnd = DateTime.Today.AddDays(occupiedNumEnd);
 
-            var bookingList = new Booking[] { new Booking() { StartDate = start.AddDays(-1), EndDate = end, RoomId = 1, CustomerId = 1, IsActive = true, Id = 1 }, new Booking() { StartDate = start, EndDate = end, RoomId = 2, CustomerId = 2, IsActive = true, Id = 2 } };
+            var bookingList = new Booking[] { new Booking() { StartDate = occupiedDateStart.AddDays(-1), EndDate = occupiedDateEnd, RoomId = 1, CustomerId = 1, IsActive = true, Id = 1 }, new Booking() { StartDate = occupiedDateStart, EndDate = occupiedDateEnd, RoomId = 2, CustomerId = 2, IsActive = true, Id = 2 } };
             var roomsList = new Room[] { new Room() { Description = "1", Id = 1 }, new Room() { Description = "2", Id = 2 } };
 
             mockBookingRepository = new Mock<IRepository<Booking>>();
@@ -47,7 +47,7 @@ namespace HotelBooking.SpecflowTests.Steps
             fakeBookingManager = new BookingManager(mockBookingRepository.Object, mockRoomRepository.Object);
 
             //Dates are fully occupied in the FakeBookingRepository
-            IRepository<Booking> bookingRepository = new FakeBookingRepository(occupiedStartDate, occupiedEndDate);
+            IRepository<Booking> bookingRepository = new FakeBookingRepository(occupiedDateStart, occupiedDateEnd);
             IRepository<Room> roomRepository = new FakeRoomRepository();
             bookingManager = new BookingManager(bookingRepository, roomRepository);
 
@@ -56,25 +56,23 @@ namespace HotelBooking.SpecflowTests.Steps
         [Given(@"start date before occupied time")]
         public void GivenStartDateBeforeOccupiedTime()
         {
-            //start = DateTime.Today.AddDays(10 - 3);
-            start = occupiedStartDate.AddDays(-3);
+            occupiedDateStart = DateTime.Today.AddDays(occupiedNumStart - 3);
         }
-        
+
         [Given(@"end date before occupied time")]
         public void GivenEndDateBeforeOccupiedTime()
         {
-            //end = DateTime.Today.AddDays(10 - 2);
-            end = occupiedEndDate.AddDays(- 2);
+            occupiedDateEnd = DateTime.Today.AddDays(occupiedNumStart - 2);
         }
-        
+
         [When(@"creating a booking")]
         public void WhenCreatingABooking()
         {
-            Booking booking = new Booking() { CustomerId = customerId, StartDate = start, EndDate = end, RoomId = roomID, Id = id };
+            Booking booking = new Booking() { CustomerId = customerId, StartDate = occupiedDateStart, EndDate = occupiedDateEnd, RoomId = roomID, Id = id };
 
             result = fakeBookingManager.CreateBooking(booking);
         }
-        
+
         [Then(@"the booking should be created")]
         public void ThenTheBookingShouldBeCreated()
         {
@@ -84,15 +82,13 @@ namespace HotelBooking.SpecflowTests.Steps
         [Given(@"start date after occupied time")]
         public void GivenStartDateAfterOccupiedTime()
         {
-            //start = DateTime.Today.AddDays(20 + 2);
-            start = occupiedStartDate.AddDays(12);
+            occupiedDateStart = DateTime.Today.AddDays(occupiedNumEnd + 2);
         }
 
         [Given(@"end date after occupied time")]
         public void GivenEndDateAfterOccupiedTime()
         {
-            //end = DateTime.Today.AddDays(20 + 3);
-            end = occupiedEndDate.AddDays(13);
+            occupiedDateEnd = DateTime.Today.AddDays(occupiedNumEnd + 3);
         }
 
         [Then(@"the booking should not be created")]
@@ -104,23 +100,19 @@ namespace HotelBooking.SpecflowTests.Steps
         [Given(@"start date in occupied time")]
         public void GivenStartDateInOccupiedTime()
         {
-            //start = DateTime.Today.AddDays(10);
-            start = occupiedStartDate.AddDays(1);
-
+            occupiedDateStart = DateTime.Today.AddDays(occupiedNumStart);
         }
 
         [Given(@"end date in occupied time")]
         public void GivenEndDateInOccupiedTime()
         {
-            //end = DateTime.Today.AddDays(20);
-            end = occupiedEndDate.AddDays(5);
+            occupiedDateEnd = DateTime.Today.AddDays(occupiedNumEnd);
         }
 
         [Given(@"start date after, and > end date")]
         public void GivenStartDateAfterAndEndDate()
         {
-            //start = DateTime.Today.AddDays(10 + 4);
-            start = occupiedEndDate.AddDays(1);
+            occupiedDateStart = DateTime.Today.AddDays(occupiedNumStart + 4);
         }
 
     }
